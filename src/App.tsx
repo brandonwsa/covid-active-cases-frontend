809d474fe5690f9f0b647d66362f7eb2a0600131
Brandon Ahrens <b-ahrens@wiu.edu>
2021-01-26 14:18:53 -0600

    Added proper display set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Nav/>
+      
+      <div className="container-fluid">
+        <div className="row">
+      
+          <main role="main" className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      
+            <h2>Chart goes here</h2>
+            <canvas className="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+      
+            <h2>Section title</h2>
+            <div className="table-responsive">
+              <table className="table table-striped table-sm">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                    <th>Header</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1,001</td>
+                    <td>Lorem</td>
+                    <td>ipsum</td>
+                    <td>dolor</td>
+                    <td>sit</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
