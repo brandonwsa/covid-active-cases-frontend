b7bdc0ce0b579b89bb1e4aa7b6d7d329da3379b9
Brandon Ahrens <b-ahrens@wiu.edu>
2021-02-06 21:07:10 -0600

    Fixed COVID-19 button on Nav bar redirecting to portfolio when deployed | had to change href='/' since it redirect to root of website

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@
 const Nav = () => {
     return (
     <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a className="navbar-brand me-0 px-3" href="/">COVID-19 Active Cases</a>
+        <a className="navbar-brand me-0 px-3" href="javascript:void(0)">COVID-19 Active Cases</a>
         <a className="portfolio me-0 px-3" href="https://brandonwsa.github.io/">My Portfolio</a>
 
     </header>
